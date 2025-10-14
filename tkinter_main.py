f7181c4645ba6e463a34521de953699207a679de
Berat Baş <114669067+BeratBass@users.noreply.github.com>
2025-10-14 15:01:33 +0300

    Update tkinter_main.py

--- tkinter_main.py
+++ tkinter_main.py
@@ -1,5 +1,6 @@
 from tkinter import * 
 import tkinter as tk
+import os
 from cinema import cinema
 from concert import concert
 from theatre import theatre
@@ -12,6 +13,11 @@ class automationn (Frame,concert,theatre,cinema): #class oluşturulup cinema,con
         Frame.__init__(self,automation)
         self.automation=automation
 
+        # base dizin ve yardımcı fonksiyon (resim yollarını güvenli oluşturmak için)
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        def img_path(*parts):
+            return os.path.join(BASE_DIR, *parts)
+
 
 
         F1 = Frame(self.automation)#main pencereyi oluşturduk.
@@ -21,10 +27,11 @@ class automationn (Frame,concert,theatre,cinema): #class oluşturulup cinema,con
         self.automation.title("Ticket Automation")
         self.automation.geometry("1800x800") #main pencerenin boyutunu ayarladık.
 
-        bgimg= tk.PhotoImage(file = "--image--\\cinemaphoto\\backs.png") #main pencerenin arkaplanını ayarladık
-        limg= Label(windows, i=bgimg)
-        limg.image=bgimg
-        limg.place(x=0,y=0)
+        # main pencerenin arkaplanını ayarladık (tam yol kullanıyoruz)
+        bgimg = tk.PhotoImage(master=self.automation, file=img_path("--image--", "cinemaphoto", "backs.png"))
+        limg = Label(self.automation, image=bgimg)
+        limg.image = bgimg
+        limg.place(x=0, y=0)
 
     
         
@@ -76,50 +83,50 @@ class automationn (Frame,concert,theatre,cinema): #class oluşturulup cinema,con
         
 
         #cinemaya ait pngleri ekledik. 
-        filmimg=PhotoImage(file="--image--\\cinemaphoto\\arog.png")
-        filmbutton=tk.Button(windows,image=filmimg,command=openweb)
-        filmbutton.image=filmimg
-        filmbutton.place(x=200,y=325,width=125,height=125)
+        filmimg = PhotoImage(master=self.automation, file=img_path("--image--", "cinemaphoto", "Arog.png"))
+        filmbutton = tk.Button(self.automation, image=filmimg, command=openweb)
+        filmbutton.image = filmimg
+        filmbutton.place(x=200, y=325, width=125, height=125)
 
-        filmimg2=PhotoImage(file="--image--\\cinemaphoto\\avatar_2.png")
-        filmbutton2=tk.Button(windows,image=filmimg2,command=openweb1)
-        filmbutton2.image=filmimg2
-        filmbutton2.place(x=320,y=325,width=125,height=125)
+        filmimg2 = PhotoImage(master=self.automation, file=img_path("--image--", "cinemaphoto", "Avatar_2.png"))
+        filmbutton2 = tk.Button(self.automation, image=filmimg2, command=openweb1)
+        filmbutton2.image = filmimg2
+        filmbutton2.place(x=320, y=325, width=125, height=125)
 
-        filmimg3=PhotoImage(file="--image--\\cinemaphoto\\Babam_Ve_Oğlum.png")
-        filmbutton3=tk.Button(windows,image=filmimg3,command=openweb2)
-        filmbutton3.image=filmimg3
-        filmbutton3.place(x=440,y=325,width=125,height=125)
+        filmimg3 = PhotoImage(master=self.automation, file=img_path("--image--", "cinemaphoto", "Babam_Ve_Oğlum.png"))
+        filmbutton3 = tk.Button(self.automation, image=filmimg3, command=openweb2)
+        filmbutton3.image = filmimg3
+        filmbutton3.place(x=440, y=325, width=125, height=125)
 
-        filmimg4=PhotoImage(file="--image--\\cinemaphoto\\Gora.png")
-        filmbutton4=tk.Button(windows,image=filmimg4,command=openweb3)
-        filmbutton4.image=filmimg4
-        filmbutton4.place(x=200,y=475,width=125,height=125)
+        filmimg4 = PhotoImage(master=self.automation, file=img_path("--image--", "cinemaphoto", "Gora.png"))
+        filmbutton4 = tk.Button(self.automation, image=filmimg4, command=openweb3)
+        filmbutton4.image = filmimg4
+        filmbutton4.place(x=200, y=475, width=125, height=125)
 
-        filmimg5=PhotoImage(file="--image--\\cinemaphoto\\Issız_Adam.png")
-        filmbutton5=tk.Button(windows,image=filmimg5,command=openweb4)
-        filmbutton5.image=filmimg5
-        filmbutton5.place(x=320,y=475,width=125,height=125)
+        filmimg5 = PhotoImage(master=self.automation, file=img_path("--image--", "cinemaphoto", "Issız_Adam.png"))
+        filmbutton5 = tk.Button(self.automation, image=filmimg5, command=openweb4)
+        filmbutton5.image = filmimg5
+        filmbutton5.place(x=320, y=475, width=125, height=125)
 
-        filmimg6=PhotoImage(file="--image--\\cinemaphoto\\kolpacino.png")
-        filmbutton6=tk.Button(windows,image=filmimg6,command=openweb5)
-        filmbutton6.image=filmimg6
-        filmbutton6.place(x=440,y=475,width=125,height=125)
+        filmimg6 = PhotoImage(master=self.automation, file=img_path("--image--", "cinemaphoto", "kolpacino.png"))
+        filmbutton6 = tk.Button(self.automation, image=filmimg6, command=openweb5)
+        filmbutton6.image = filmimg6
+        filmbutton6.place(x=440, y=475, width=125, height=125)
 
-        filmimg7=PhotoImage(file="--image--\\cinemaphoto\\Kurtlar_Vadisi_Irak.png")
-        filmbutton7=tk.Button(windows,image=filmimg7,command=openweb6)
-        filmbutton7.image=filmimg7
-        filmbutton7.place(x=200,y=625,width=125,height=125)
+        filmimg7 = PhotoImage(master=self.automation, file=img_path("--image--", "cinemaphoto", "Kurtlar_Vadisi_Irak.png"))
+        filmbutton7 = tk.Button(self.automation, image=filmimg7, command=openweb6)
+        filmbutton7.image = filmimg7
+        filmbutton7.place(x=200, y=625, width=125, height=125)
 
-        filmimg8=PhotoImage(file="--image--\\cinemaphoto\\Maskeli_Besler_Irak.png")
-        filmbutton8=tk.Button(windows,image=filmimg8,command=openweb7)
-        filmbutton8.image=filmimg8
-        filmbutton8.place(x=320,y=625,width=125,height=125)
+        filmimg8 = PhotoImage(master=self.automation, file=img_path("--image--", "cinemaphoto", "Maskeli_Besler_Irak.png"))
+        filmbutton8 = tk.Button(self.automation, image=filmimg8, command=openweb7)
+        filmbutton8.image = filmimg8
+        filmbutton8.place(x=320, y=625, width=125, height=125)
 
-        filmimg9=PhotoImage(file="--image--\\cinemaphoto\\Recepİvedik_7.png")
-        filmbutton9=tk.Button(windows,image=filmimg9,command=openweb8)
-        filmbutton9.image=filmimg9
-        filmbutton9.place(x=440,y=625,width=125,height=125)       
+        filmimg9 = PhotoImage(master=self.automation, file=img_path("--image--", "cinemaphoto", "Recepİvedik_7.png"))
+        filmbutton9 = tk.Button(self.automation, image=filmimg9, command=openweb8)
+        filmbutton9.image = filmimg9
+        filmbutton9.place(x=440, y=625, width=125, height=125)       
         
         #THEATRE
         #thatre ait gösterim linklerini ekledik.
@@ -132,24 +139,26 @@ class automationn (Frame,concert,theatre,cinema): #class oluşturulup cinema,con
             webbrowser.open(url2_1,new=new) 
         def theatreweb2():
             webbrowser.open(url3_1,new=new)
-        example_show_text=Label(text="Click the buttons for sample shows",font=1,bg="lavenderblush").place(x=640,y=750)
-        show_text=Label(text="Shows:",font=20,bg="lavenderblush").place(x=600,y=280)
+        example_show_text = Label(self.automation, text="Click the buttons for sample shows", font=1, bg="lavenderblush")
+        example_show_text.place(x=640, y=750)
+        show_text = Label(self.automation, text="Shows:", font=20, bg="lavenderblush")
+        show_text.place(x=600, y=280)
 
         #theatre ait pngleri ekledik.
-        theatreimg=PhotoImage(file="--image--\\theatrephoto\\cmylmz.png")
-        theatrebutton=tk.Button(windows,image=theatreimg,command=theatreopenweb)
-        theatrebutton.image=theatreimg
-        theatrebutton.place(x=700,y=325,width=125,height=175)
+        theatreimg = PhotoImage(master=self.automation, file=img_path("--image--", "theatrephoto", "cmylmz.png"))
+        theatrebutton = tk.Button(self.automation, image=theatreimg, command=theatreopenweb)
+        theatrebutton.image = theatreimg
+        theatrebutton.place(x=700, y=325, width=125, height=175)
 
-        theatreimg2=PhotoImage(file="--image--\\theatrephoto\\konusanlar.png")
-        theatrebutton2=tk.Button(windows,image=theatreimg2,command=theatreweb1)
-        theatrebutton2.image=theatreimg2
-        theatrebutton2.place(x=700,y=490,width=125,height=125)
+        theatreimg2 = PhotoImage(master=self.automation, file=img_path("--image--", "theatrephoto", "konusanlar.png"))
+        theatrebutton2 = tk.Button(self.automation, image=theatreimg2, command=theatreweb1)
+        theatrebutton2.image = theatreimg2
+        theatrebutton2.place(x=700, y=490, width=125, height=125)
         
-        theatreimg3=PhotoImage(file="--image--\\theatrephoto\\tolga.png")
-        theatrebutton3=tk.Button(windows,image=theatreimg3,command=theatreweb2)
-        theatrebutton3.image=theatreimg3
-        theatrebutton3.place(x=700,y=625,width=125,height=125)
+        theatreimg3 = PhotoImage(master=self.automation, file=img_path("--image--", "theatrephoto", "tolga.png"))
+        theatrebutton3 = tk.Button(self.automation, image=theatreimg3, command=theatreweb2)
+        theatrebutton3.image = theatreimg3
+        theatrebutton3.place(x=700, y=625, width=125, height=125)
 
         #CONCERTİMAGE
         #concert ait videoları ekledik
@@ -172,40 +181,42 @@ class automationn (Frame,concert,theatre,cinema): #class oluşturulup cinema,con
             webbrowser.open(url5_2,new=new)
         def concertopenweb5():
             webbrowser.open(url6_2,new=new)                    
-        sing_text=Label(text="Click the buttons for sample concert",font=1,bg="lavenderblush").place(x=1100,y=750)
+        sing_text = Label(self.automation, text="Click the buttons for sample concert", font=1, bg="lavenderblush")
+        sing_text.place(x=1100, y=750)
 
-        singer_text=Label(text="Singers:",font=20,bg="lavenderblush").place(x=1000,y=280)
+        singer_text = Label(self.automation, text="Singers:", font=20, bg="lavenderblush")
+        singer_text.place(x=1000, y=280)
         
         #concert ait pngleri ekledik
-        concertimg=PhotoImage(file="--image--\\concertphoto\\duman.png")
-        concertbutton=tk.Button(windows,image=concertimg,command=concertopenweb)
-        concertbutton.image=concertimg
-        concertbutton.place(x=1000,y=325,width=150,height=150)
+        concertimg = PhotoImage(master=self.automation, file=img_path("--image--", "concertphoto", "duman.png"))
+        concertbutton = tk.Button(self.automation, image=concertimg, command=concertopenweb)
+        concertbutton.image = concertimg
+        concertbutton.place(x=1000, y=325, width=150, height=150)
 
-        concertimg2=PhotoImage(file="--image--\\concertphoto\\manga.png")
-        concertbutton2=tk.Button(windows,image=concertimg2,command=concertopenweb1)
-        concertbutton2.image=concertimg2
-        concertbutton2.place(x=1160,y=325,width=150,height=150)
+        concertimg2 = PhotoImage(master=self.automation, file=img_path("--image--", "concertphoto", "manga.png"))
+        concertbutton2 = tk.Button(self.automation, image=concertimg2, command=concertopenweb1)
+        concertbutton2.image = concertimg2
+        concertbutton2.place(x=1160, y=325, width=150, height=150)
 
-        concertimg3=PhotoImage(file="--image--\\concertphoto\\şanışer.png")
-        concertbutton3=tk.Button(windows,image=concertimg3,command=concertopenweb2)
-        concertbutton3.image=concertimg3
-        concertbutton3.place(x=1320,y=325,width=150,height=150)
+        concertimg3 = PhotoImage(master=self.automation, file=img_path("--image--", "concertphoto", "şanışer.png"))
+        concertbutton3 = tk.Button(self.automation, image=concertimg3, command=concertopenweb2)
+        concertbutton3.image = concertimg3
+        concertbutton3.place(x=1320, y=325, width=150, height=150)
 
-        concertimg4=PhotoImage(file="--image--\\concertphoto\\sezenaksu.png")
-        concertbutton4=tk.Button(windows,image=concertimg4,command=concertopenweb3)
-        concertbutton4.image=concertimg4
-        concertbutton4.place(x=1160,y=525,width=150,height=150)
+        concertimg4 = PhotoImage(master=self.automation, file=img_path("--image--", "concertphoto", "sezenaksu.png"))
+        concertbutton4 = tk.Button(self.automation, image=concertimg4, command=concertopenweb3)
+        concertbutton4.image = concertimg4
+        concertbutton4.place(x=1160, y=525, width=150, height=150)
 
-        concertimg5=PhotoImage(file="--image--\\concertphoto\\yalın.png")
-        concertbutton5=tk.Button(windows,image=concertimg5,command=concertopenweb4)
-        concertbutton5.image=concertimg5
-        concertbutton5.place(x=1000,y=525,width=150,height=150)
+        concertimg5 = PhotoImage(master=self.automation, file=img_path("--image--", "concertphoto", "yalın.png"))
+        concertbutton5 = tk.Button(self.automation, image=concertimg5, command=concertopenweb4)
+        concertbutton5.image = concertimg5
+        concertbutton5.place(x=1000, y=525, width=150, height=150)
 
-        concertimg6=PhotoImage(file="--image--\\concertphoto\\yıldız tilbe.png")
-        concertbutton6=tk.Button(windows,image=concertimg6,command=concertopenweb5)
-        concertbutton6.image=concertimg6
-        concertbutton6.place(x=1320,y=525,width=150,height=150)
+        concertimg6 = PhotoImage(master=self.automation, file=img_path("--image--", "concertphoto", "yıldız tilbe.png"))
+        concertbutton6 = tk.Button(self.automation, image=concertimg6, command=concertopenweb5)
+        concertbutton6.image = concertimg6
+        concertbutton6.place(x=1320, y=525, width=150, height=150)
         
         
        
